fb6085103713182678c6e332c604eef02f2f7229
Konstantinos <k.tzavellas@hotmail.com>
2019-05-08 02:48:08 +0200

    Commit changes

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -96,24 +96,25 @@ $(document).ready(function() {
         .text()
         .replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>")
     );
-    anime
-      .timeline({ loop: true })
-      .add({
-        targets: ".ml10 .letter",
-        rotateY: [-90, 0],
-        duration: 3000,
-        delay: function(el, i) {
-          return 45 * i;
-        }
-      })
-      .add({
-        targets: ".ml10",
-        opacity: 1,
-        duration: 1000,
-        easing: "easeOutExpo",
-        delay: 10000
-      });
   });
+
+  anime
+    .timeline({ loop: true })
+    .add({
+      targets: ".ml10 .letter",
+      rotateY: [-90, 0],
+      duration: 1300,
+      delay: function(el, i) {
+        return 45 * i;
+      }
+    })
+    .add({
+      targets: ".ml10",
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 10000
+    });
 
   // Check if element is scrolled into view
   function isScrolledIntoView(elem) {
