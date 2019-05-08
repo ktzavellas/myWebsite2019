2e2ddfe2747e1d498e534a5ff0153f1b925686f0
Konstantinos <k.tzavellas@hotmail.com>
2019-05-08 02:54:16 +0200

    Commit changes

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -90,7 +90,7 @@ $(document).ready(function() {
   });
 
   /* Code to animate the word "hello" in the header */
-  $(".ml10 .letters").each(function() {
+  /* $(".ml10 .letters").each(function() {
     $(this).html(
       $(this)
         .text()
@@ -114,7 +114,7 @@ $(document).ready(function() {
       duration: 1000,
       easing: "easeOutExpo",
       delay: 10000
-    });
+    }); */
 
   // Check if element is scrolled into view
   function isScrolledIntoView(elem) {
